a10ce830eed0fbdf478e15ee41bb66b77cf22212
joshua-rigg <joshua.rigg@stu.ocsb.ca>
2026-02-23 13:26:01 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Joshua
+ * Created on: Feb 2026
+ * This program wil say what the temperature is
 */
 
-basic.showString('Hello, World!')
+let temperature: number
+
+//reset
+basic.clearScreen()
+basic.showIcon(IconNames.Sad)
+
+// Temperature will show when A button is pressed
+input.onButtonPressed(Button.A, function () {
+    temperature = input.temperature()
+    basic.showString("The temperature is")
+    basic.showNumber(temperature)
+    pause(1000)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Sad)
+})
