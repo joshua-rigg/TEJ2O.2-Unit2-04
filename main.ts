651329b238e9e40d965bfbe65f3ddf0bdf7ef3cf
joshua-rigg <joshua.rigg@stu.ocsb.ca>
2026-02-26 13:17:38 -0500

    changed comments

--- main.ts
+++ main.ts
@@ -2,21 +2,20 @@
  *
  * Created by: Joshua
  * Created on: Feb 2026
- * This program wil say what the temperature is
+ * This program will say what the temperature is
 */
 
 let temperature: number
 
 //reset
 basic.clearScreen()
-basic.showIcon(IconNames.Sad)
+basic.showIcon(IconNames.Happy)
 
 // Temperature will show when A button is pressed
 input.onButtonPressed(Button.A, function () {
     temperature = input.temperature()
-    basic.showString("The temperature is")
-    basic.showNumber(temperature)
+    basic.showString("The temperature is: " + temperature + " C")
     pause(1000)
     basic.clearScreen()
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Happy)
 })
